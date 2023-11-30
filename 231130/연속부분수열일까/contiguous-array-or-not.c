73bbaeacc7d9a96a1e714df800f4b6dc7c58ea98
pakr04 <150348820+pakr04@users.noreply.github.com>
2023-11-30 20:50:50 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231130/연속부분수열일까/contiguous-array-or-not.c
+++ 231130/연속부분수열일까/contiguous-array-or-not.c
@@ -10,7 +10,7 @@ void input(int *p, int k)
     }
 }
 
-int fun(int *x, int *y, int n, int m)
+int isSubset(int *x, int *y, int n, int m)
 {
     int i, j, start;
     for (i = 0; i < n; i++)
@@ -18,21 +18,25 @@ int fun(int *x, int *y, int n, int m)
         if (x[i] == y[0])
         {
             start = i;
-            break;
+            for (j = 0; j < m && i + j < n; j++)
+            {
+                if (x[i + j] != y[j])
+                {
+                    return 0;
+                }
+            }
+            if (j == m)
+            {
+                return 1;
+            }
         }
     }
-    if (n - start < m - 1)
-        return 0;
-    j = 1;
-    for (i = start + 1; i < n; i++, j++)
-        if (x[i] != y[j])
-            return 0;
-    return 1;
+    return 0;
 }
 
 int main()
 {
-    int n, m, i, j, *p1, *p2, sw;
+    int n, m, *p1, *p2, sw;
     scanf("%d %d", &n, &m);
 
     if (n <= 0 || m <= 0)
@@ -52,7 +56,7 @@ int main()
 
     input(p1, n);
     input(p2, m);
-    sw = fun(p1, p2, n, m);
+    sw = isSubset(p1, p2, n, m);
 
     if (sw == 1)
         printf("Yes\n");
